0119e1e05420ada93293845044d5f3017ed27936
Tony-Wei Feng <tony.w.feng@gmail.com>
2014-01-06 02:50:05 +0800

    The Original

---
+++ DayCounter/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace DayCounter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TB_year = new System.Windows.Forms.TextBox();
+            this.TB_month = new System.Windows.Forms.TextBox();
+            this.TB_day = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.L_result = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(131, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please input your birthday:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Year";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Month";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(183, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Day";
+            // 
+            // TB_year
+            // 
+            this.TB_year.Location = new System.Drawing.Point(47, 42);
+            this.TB_year.Name = "TB_year";
+            this.TB_year.Size = new System.Drawing.Size(53, 20);
+            this.TB_year.TabIndex = 4;
+            // 
+            // TB_month
+            // 
+            this.TB_month.Location = new System.Drawing.Point(149, 42);
+            this.TB_month.Name = "TB_month";
+            this.TB_month.Size = new System.Drawing.Size(28, 20);
+            this.TB_month.TabIndex = 5;
+            // 
+            // TB_day
+            // 
+            this.TB_day.Location = new System.Drawing.Point(215, 42);
+            this.TB_day.Name = "TB_day";
+            this.TB_day.Size = new System.Drawing.Size(28, 20);
+            this.TB_day.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(197, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 111);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(133, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "You already have lived for ";
+            // 
+            // L_result
+            // 
+            this.L_result.AutoSize = true;
+            this.L_result.Location = new System.Drawing.Point(146, 111);
+            this.L_result.Name = "L_result";
+            this.L_result.Size = new System.Drawing.Size(0, 13);
+            this.L_result.TabIndex = 9;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(183, 111);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(31, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Days";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 137);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.L_result);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TB_day);
+            this.Controls.Add(this.TB_month);
+            this.Controls.Add(this.TB_year);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TB_year;
+        private System.Windows.Forms.TextBox TB_month;
+        private System.Windows.Forms.TextBox TB_day;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label L_result;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ DayCounter/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DayCounter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ye = Convert.ToInt32(TB_year.Text);
+            int mo = Convert.ToInt32(TB_month.Text);
+            int da = Convert.ToInt32(TB_day.Text);
+
+            L_result.Text = dayCount(ye,mo,da).ToString();
+            
+        }
+
+        static int dayCount(int y, int m, int d)
+        {
+            DateTime startDate = new DateTime(y,m,d);
+            TimeSpan differ = DateTime.Now.Subtract(startDate);
+            //int year = y;
+            //int month = m;
+            //int day = d;
+            
+            int result = differ.Days;
+
+            return result;
+        }
+
+    }
+}
